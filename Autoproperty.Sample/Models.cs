4daee89c4ff2ab5c9695a8bed6e7d539ab21bd3e
Yuriy Lyeshchenko <yuriydev@live.com>
2024-06-26 10:29:24 -0500

    basic implementation of partial class generation

--- Autoproperty.Sample/Models.cs
+++ Autoproperty.Sample/Models.cs
@@ -5,7 +5,7 @@ public interface IAuditMetadata
     DateTimeOffset LastUpdated { get; set; }
 }
 
-public record Book : IAuditMetadata
+public partial record Book : IAuditMetadata
 {
     public required string Title { get; set; }
 
