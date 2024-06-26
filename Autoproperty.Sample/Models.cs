cb52e5925e211f929290ca3d4d46e46560aa2c70
Yuriy Lyeshchenko <yuriydev@live.com>
2024-06-26 11:17:35 -0500

    more examples

--- Autoproperty.Sample/Models.cs
+++ Autoproperty.Sample/Models.cs
@@ -1,15 +1,35 @@
 ﻿namespace Autoproperty.Sample;
 
+public interface IHasId
+{
+    public int Id { get; set; }
+}
+
+public interface IHasTitle
+{
+    public string Title { get; set; }
+}
+
+public interface IHasActiveFlag
+{
+    public bool IsActive { get; set; }
+}
+
 public interface IAuditMetadata
 {
     DateTimeOffset LastUpdated { get; set; }
+
+    bool IsEditable { get; }
 }
 
-public partial class Book : IAuditMetadata
+public partial class Author : IHasId, IHasActiveFlag, IAuditMetadata
+{
+    public required string Name { get; set; }
+}
+
+public partial class Book : IHasId, IHasTitle, IHasActiveFlag, IAuditMetadata
 {
     public required string Title { get; set; }
 
     public required string Author { get; set; }
-
-    //public DateTimeOffset LastUpdated { get; set; }
 }
